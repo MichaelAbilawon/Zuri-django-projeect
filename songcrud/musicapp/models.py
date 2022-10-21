d542b33ec8ba5ac95e5b9977fcaf371a7d3df861
MichaelAbilawon <mabilawon@gmail.com>
2022-10-21 17:54:25 +0100

    new commit by 5:54pm

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    age = models.IntegerField
+
+class Song(models.Model):
+    title = models.CharField(max_length=200)
+    date_released = models.DateField
+    likes = models.IntegerField
+    artiste_id = models.IntegerField
+
+class Lyric(models.Model):
+    content = models.CharField
+    song_id = models.IntegerField
