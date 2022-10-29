a4e6330d128f5851ddcfae8f10d811205e684dec
MichaelAbilawon <mabilawon@gmail.com>
2022-10-29 04:21:37 +0100

    week 5 task

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -5,14 +5,25 @@ from django.db import models
 class Artiste(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    age = models.IntegerField
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name
 
 class Song(models.Model):
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    date_released = models.DateField
-    likes = models.IntegerField
-    artiste_id = models.IntegerField
+    date_released = models.DateField()
+    likes = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.title
+
 
 class Lyric(models.Model):
-    content = models.CharField
-    song_id = models.IntegerField
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.TextField()
+    
+    def __str__(self):
+        return self.content
+    
